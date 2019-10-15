82e3f54e9bc61aca1b9b4c8cf9c1b8de30560b80
Hunter Thompson <hthompson@a10networks.com>
2019-10-15 20:45:54 +0000

    Updated url building

--- misty_client/base.py
+++ misty_client/base.py
@@ -10,12 +10,15 @@ class Client(object):
         return request.get(url, params=params)
 
     def post(self, url, payload, params={}, **kwargs):
+        url = "{}{}".format(self.api_base, url)
         return request.post(url, params=params, data=payload)
 
     def put(self, url, payload, params={}, **kwargs):
+        url = "{}{}".format(self.api_base, url)
         return request.put(url, params=params, data=payload)
 
     def delete(self, url, params={}, **kwargs):
+        url = "{}{}".format(self.api_base, url)
         return request.delete(url, params=params)
 
 class Base(object):
